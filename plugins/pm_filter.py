4dff4830a2aa5d73ec941fb147f2c483023228db
crazyspiderat3 <112283064+crazyspiderat3@users.noreply.github.com>
2022-10-02 12:08:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -72,7 +72,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"◆{get_size(file.file_size)}➢ {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"◆{get_size(file.file_size)}» {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -84,7 +84,7 @@ async def next_page(bot, query):
                     text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"◆{get_size(file.file_size)}➢",
+                    text=f"◆{get_size(file.file_size)}»",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -760,7 +760,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"◆{get_size(file.file_size)}➢ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"◆{get_size(file.file_size)}» {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -773,7 +773,7 @@ async def auto_filter(client, msg, spoll=False):
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"◆{get_size(file.file_size)}➢",
+                    text=f"◆{get_size(file.file_size)}»",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
